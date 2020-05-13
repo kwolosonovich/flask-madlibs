b3154bc4c9dae3bc539fe3e48701eba0125a50a3
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-05-13 15:30:17 -0700

    added variables to app.py file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #import flask and access to requests
 from flask import Flask, request
 from flask import Flask, render_template, request
+from stories import story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
@@ -9,9 +10,12 @@ app.config['SECRET_KEY'] = "secret"
 @app.route('/')
 def index():
     """Return homepage."""
-    return render_template("home.html")
+    # story = Story()
+    prompts = story.prompts
+    return render_template("home.html", prompts=prompts)
 
-@app.route('/stories')
+@app.route('/story')
 def my_stories():
     """stories page"""
-    return render_template("stories.html")
+    text = story.generate(request.args)
+    return render_template("stories.html", text=text)
