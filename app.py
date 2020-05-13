5324ecef948076bf51ca5eb5cc4304a2e595c13d
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-05-13 13:14:15 -0700

    added template code to stories and home files

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 #import flask and access to requests
 from flask import Flask, request
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
 
 @app.route('/')
 def index():
     """Return homepage."""
     return render_template("home.html")
+
+@app.route('/stories')
+def my_stories():
+    """stories page"""
+    return render_template("stories.html")
