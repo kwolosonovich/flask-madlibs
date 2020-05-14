0abdf8257da698d5f6713769333d956e2b96cb60
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-05-14 10:03:26 -0700

    resolved merge conflicts

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 #import flask and access to requests
 from flask import Flask, request
+from flask import Flask, render_template, request
+from stories import story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
 
 @app.route('/')
 def index():
-    """Return homepage."""
-    return render_template("home.html")
+    """Return homepage with questions form story"""
+    prompts = story.prompts
+    return render_template("home.html", prompts=prompts)
+
+@app.route('/story')
+def my_stories():
+    """stories page"""
+    text = story.generate(request.args)
+    return render_template("story.html", text=text)
